6395c65effeb031d29bea8aeb55eb52e8e76cde5
Flor de Liz Velazque Quispe <fvelazque@unsa.edu.pe>
2025-01-20 09:32:52 -0500

    ejercicios d ela primera semana

---
+++ semana1/ejercicio1.py
@@ -0,0 +1,6 @@
+#El usuario ingresa un número, determine si el número es 31
+numero = int(input ("Ingresar numero: "))
+if numero==31:
+    print("El numero es 31")
+else :
+    print("El numero es diferente de 31")
